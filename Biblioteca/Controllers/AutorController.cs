c81aeec830af979ac65ddc63f983bbfff97f8526
Renan Costa da Silva <renancs93@gmail.com>
2020-11-19 19:44:28 -0300

    Organização e Crud de Autor

---
+++ Biblioteca/Controllers/AutorController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Biblioteca.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+        private readonly ISession _session;
+
+        public AutorController(ISession session)
+        {
+            _session = session;
+        }
+
+        // GET: api/<AutorController>
+        [HttpGet]
+        public IEnumerable<Autor> Get()
+        {
+            return _session.Query<Autor>().ToArray();
+        }
+
+        // GET api/<AutorController>/5
+        [HttpGet("{id}")]
+        public Autor Get(int id)
+        {
+            return _session.Query<Autor>().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST api/<AutorController>
+        [HttpPost]
+        public async Task<ActionResult<Autor>> Post([FromBody] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ITransaction transaction = _session.BeginTransaction())
+                {
+                    await _session.SaveAsync(autor);
+                    await transaction.CommitAsync();
+                    return autor;
+                }
+            }
+            return autor;
+        }
+
+        // PUT api/<AutorController>/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Autor>> Put(int id, [FromBody] Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                using (ITransaction transaction = _session.BeginTransaction())
+                {
+                    // Verifica se existe o objeto a ser atualizado
+                    var item = Get(id);// await _session.GetAsync<Autor>(id);
+
+                    if(item == null)
+                        return NotFound();
+
+                    autor.Id = id;
+                    await _session.MergeAsync(autor);
+                    await transaction.CommitAsync();
+                    return autor;
+                                       
+                }
+            }
+            return autor;
+        }
+
+        // DELETE api/<AutorController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            using (ITransaction transaction = _session.BeginTransaction())
+            {
+                // Carrega o objeto a ser deletado
+                var item = await _session.GetAsync<Autor>(id);
+
+                if(item != null)
+                {
+                    await _session.DeleteAsync(item);
+                    await transaction.CommitAsync();
+                }
+                return Ok();
+            }
+        }
+    }
+}
