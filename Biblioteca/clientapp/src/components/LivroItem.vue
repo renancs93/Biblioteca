e52155176448f967c1bcce3b99ca83d50c8d0f98
Renan Costa da Silva <renancs93@gmail.com>
2020-11-21 00:09:45 -0300

    Layout Home e Componente de Livros

---
+++ Biblioteca/clientapp/src/components/LivroItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="card">
+    <div class="conteudo">
+      <div class="titulo">
+        {{ livro.nome }}
+      </div>
+      <div class="autor">
+        Autor:
+        {{ livro.autor != null ? livro.autor.nome : "Autor Desconhecido" }}
+      </div>
+      <div class="estoque">Estoque: {{ livro.qtdEstoque }}</div>
+
+      <button class="button">COMPRAR</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LivroItem",
+  props: {
+    livro: Object,
+  },
+};
+</script>
+
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.2s;
+  border-radius: 5px;
+  padding: 10px;
+  margin: 5px;
+  width: 150px;
+  height: 200px;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+}
+.container {
+  padding: 2px 16px;
+}
+
+.conteudo {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  text-align: center;
+}
+
+.titulo {
+  font-weight: bold;
+  font-size: medium;
+}
+
+.autor {
+  font-style: italic;
+}
+
+.estoque {
+  font-size: small;
+}
+.button{
+    background-color:#ac7339;
+    padding: 10px 15px;
+    border-radius: 5px;
+    font-weight: bold;
+    color: white;
+}
+
+.button:hover{
+    filter: drop-shadow(1px 1px 3px black);
+    cursor: pointer;
+}
+
+</style>
--- Biblioteca/clientapp/src/views/Home.vue
+++ Biblioteca/clientapp/src/views/Home.vue
@@ -3,8 +3,8 @@
     <Header></Header>
     <div class="home">
       <h1>Listagem de Livros</h1>
-      <div v-for="item in livros" :key="item.id">
-          {{ item.nome }}
+      <div class="lista-livros">
+        <LivroItem :livro="item" v-for="item in livros" :key="item.id"></LivroItem>
       </div>
     </div>
   </div>
@@ -12,30 +12,41 @@
 
 <script>
 import Header from "@/components/Header.vue";
+import LivroItem from "@/components/LivroItem.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
+    LivroItem,
   },
   data() {
     return {
       livros: [],
     };
   },
-
+  methods: {
+    // http://localhost:50598/
+    carregarDados: function () {
+      fetch("http://localhost:50598/api/livro")
+        .then((response) => response.json())
+        .then((data) => {
+          window.console.log(data);
+          this.livros = data;
+        });
+    },
+  },
   created() {
-    this.livros = [
-        {
-            nome: "Teste1"
-        },
-        {
-            nome: "Teste1"
-        },
-        {
-            nome: "Teste1"
-        },
-    ]
+    this.carregarDados();
   },
 };
 </script>
+
+<style>
+.home {
+  margin: 10px;
+}
+.lista-livros {
+  display: flex;
+}
+</style>
